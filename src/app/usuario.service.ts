3a01de7ed4ea813674f1a57d35115939baa577a7
Thiego <thiego@flexpeak.com.br>
2020-08-15 01:21:49 -0400

    listando usuarios

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -8,12 +8,33 @@ export class UsuarioService {
 
   constructor() { }
 
-  getUsuario(): Usuario {
+  public getUsuario(): Usuario {
     let usuario = new Usuario()
     usuario.nome = "Thiego"
     usuario.email = "thiegobarros@gmail.com"
 
     return usuario
+  }
+
+  public listaUsuario():Usuario[] {
+    return [
+      {
+        nome: "Thiego",
+        email: "thiego@gmail.com"
+      },
+      {
+        nome: "Italo",
+        email: "italo@gmail.com"
+      },
+      {
+        nome: "Santos",
+        email: "santos@gmail.com"
+      },
+      {
+        nome: "Barros",
+        email: "barros@gmail.com"
+      }
+    ]
   }
 
 }
